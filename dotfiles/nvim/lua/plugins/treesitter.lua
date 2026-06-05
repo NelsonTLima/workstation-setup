39005ad66de90ebae3e9a7cfe8ad3ed68fdccd17
Nelson Lima <nelson@MacBook-Air-de-Nelson.local>
2026-06-05 03:48:08 -0300

    Update treesitter config

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,20 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
   config = function()
-    local config = require('nvim-treesitter.configs')
-    config.setup({
+    require("nvim-treesitter.configs").setup({
       ensure_installed = {
-        'arduino', 'bash', 'c', 'c_sharp', 'cmake', 'cpp', 'css',
-        'csv', 'dockerfile', 'git_config', 'git_rebase', 'gitattributes',
-        'gitcommit', 'gitignore', 'html', 'java', 'javascript', 'json',
-        'latex', 'lua', 'nginx', 'passwd', 'php','python', 'r', 'ruby',
-        'rust', 'sql', 'ssh_config','typescript', 'vim', 'xml','vue', 'yaml', 
+        "arduino", "bash", "c", "c_sharp", "cmake", "cpp", "css",
+        "csv", "dockerfile", "git_config", "git_rebase", "gitattributes",
+        "gitcommit", "gitignore", "html", "java", "javascript", "json",
+        "lua", "nginx", "passwd", "php", "python", "r", "ruby",
+        "rust", "sql", "ssh_config", "typescript", "vim", "xml", "vue", "yaml",
       },
       auto_install = true,
-      highlight = {enable = true},
-      indent = {enable = true},
-      incremental_selection = {enable = true}
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = { enable = true },
     })
-  end
+  end,
 }
